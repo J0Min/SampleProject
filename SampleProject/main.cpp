e998fb78832b2fa972a2d2d867561b160883dd77
J0Min <rudals489@gmail.com>
2026-04-20 14:49:40 +0900

    feat: 다양한 자료형을 활용한 캐릭터 관련 변수 선

--- SampleProject/main.cpp
+++ SampleProject/main.cpp
@@ -3,13 +3,46 @@ using namespace std;
 
 int main() 
 {
-	char userName[50] = " ";
+	char userName[50];
+	char charactorClass[50];
 
-	cout << "[ User Name ]\n";
+	int level = 1;
+	int hp = 100;
+	int mp = 100;
+	float attackSpedd = 5.5f;
+
+	int strength = 50;
+	int dexterity = 50;
+	int vitality = 50;
+	int energy = 50;
+
+	bool isHardcore = true;
+
+	cout << "[ Character Creation ]\n";
 	cout << "Input your name: ";
 	cin >> userName;
+	cout << "Input your Class; ";
+	cin >> charactorClass;
 
-	cout << "::::::: Welcome to the D&D World :::::::\n";
-	cout << "User Name: [" << userName << "]!\n";
+	cout << "\n:::::::: Welcome to the Sanctuary ::::::::\n";
+	cout << "Uset Name : [" << userName << "]\n";
+
+	cout << "----------------Character Status----------------\n";
+	cout << "Class: " << charactorClass << "\n";
+	cout << "Level: " << level << "\n";
+	cout << "HP: " << hp << "\n";
+	cout << "MP: " << mp << "\n";
+	cout << "Attack Speed: " << attackSpedd << "\n";
+	cout << "Strength: " << strength << "\n";
+	cout << "Dexterity: " << dexterity << "\n";
+	cout << "Vitality: " << vitality << "\n";
+	cout << "Energy: " << energy << "\n";
+	cout << "Haercore Mode: " << isHardcore << " (1: true, 0: false)\n";
+
+	cout << "--------------------------------\n";
+
+	cout << "[Memory Check] int type size: " << sizeof(hp) << "bytes\n";
+	cout << "[Memory Check] bool type size: " << sizeof(isHardcore) << "bytes\n";
+	
 	return 0;
 }
